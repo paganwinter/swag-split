d5c60a8b49857f83b25fed14a0173f159f48d18a
PaganWinter <whydieanut@gmail.com>
2019-08-29 14:30:01 +0530

    Fromat console log, tests

--- test/test.js
+++ test/test.js
@@ -5,11 +5,8 @@ const { split } = require('../')
 
 let splitSwag
 
-// splitSwag = split('./test/sample.yaml', 'put /resource-1, post /resource-1')
-// fs.writeFileSync('./test/sample-split.yaml', yaml.dump(splitSwag), 'utf-8')
+splitSwag = split('./test/sample.yaml', 'put /resource-1, post /resource-1', './test/sample-split.yaml')
 
-// splitSwag = split('./test/cds_full.yaml', 'get /banking/accounts/balances, post /banking/accounts/balances, get /banking/accounts/{accountId}/balance')
-// fs.writeFileSync('./test/cds_balances.yaml', yaml.dump(splitSwag), 'utf-8')
+splitSwag = split('./test/cds_full.yaml', 'get /banking/accounts/balances, post /banking/accounts/balances, get /banking/accounts/{accountId}/balance', './test/cds_balances.yaml')
 
-splitSwag = split('./test/cds_full.yaml', 'get /banking/accounts, get /banking/accounts/{accountId}')
-fs.writeFileSync('./test/cds_accounts.yaml', yaml.dump(splitSwag), 'utf-8')
+splitSwag = split('./test/cds_full.yaml', 'get /banking/accounts, get /banking/accounts/{accountId}', './test/cds_accounts.yaml')

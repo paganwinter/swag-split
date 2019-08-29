4b1d6d94e5a8b94852a8fa7f0b46d8dcc22ea35a
PaganWinter <whydieanut@gmail.com>
2019-08-29 13:47:20 +0530

    Add required nested definitions

--- test/test.js
+++ test/test.js
@@ -3,8 +3,13 @@ const yaml = require('js-yaml')
 
 const { split } = require('../')
 
-let splitSwag = split('./test/sample.yaml', 'get /resource-1, post /resource-1')
-fs.writeFileSync('./test/sample-split.yaml', yaml.dump(splitSwag), 'utf-8')
+let splitSwag
 
-splitSwag = split('./test/cds_full.yaml', 'get /banking/accounts/balances, post /banking/accounts/balances, get /banking/accounts/{accountId}/balance')
-fs.writeFileSync('./test/cds_balances.yaml', yaml.dump(splitSwag), 'utf-8')
+// splitSwag = split('./test/sample.yaml', 'put /resource-1, post /resource-1')
+// fs.writeFileSync('./test/sample-split.yaml', yaml.dump(splitSwag), 'utf-8')
+
+// splitSwag = split('./test/cds_full.yaml', 'get /banking/accounts/balances, post /banking/accounts/balances, get /banking/accounts/{accountId}/balance')
+// fs.writeFileSync('./test/cds_balances.yaml', yaml.dump(splitSwag), 'utf-8')
+
+splitSwag = split('./test/cds_full.yaml', 'get /banking/accounts, get /banking/accounts/{accountId}')
+fs.writeFileSync('./test/cds_accounts.yaml', yaml.dump(splitSwag), 'utf-8')
